7a0bb033a0741c981f4f7454b8a50a92d3d40e77
Marina Krček <marina.krcek7@gmail.com>
2022-09-09 19:18:03 +0200

    add script for plotting guessing entropy

---
+++ check_GE_results.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+model_type = "mlp"
+folder_results = f"./{model_type}"
+dataset_name = "dpa_v42"
+leakage_model = "HW"
+hiding = ""
+
+best_mse = sys.float_info.max
+best_AE = None
+best_file = None
+figure = plt.gcf()
+figure.set_size_inches(5, 4)
+for file_id in range(0, 10):
+    filepath = f"{folder_results}/{dataset_name}_{model_type}_{leakage_model}{hiding}_{file_id+1}.npz"
+    npz_file = np.load(filepath, allow_pickle=True)
+    guessing_entropy = npz_file["GE"]
+    # if mse_ds < best_mse:
+    #     best_mse = mse_ds
+    #     best_AE = npz_file
+    #     best_file = filepath
+    # hp_values = npz_file['hp_values'].item()
+    # print(hp_values, mse_ds)
+
+    plt.plot(guessing_entropy, linewidth=1)
+plt.grid(True, which="both", ls="-")
+plt.xlabel("Traces", fontsize=12)
+plt.ylabel("Guessing Entropy", fontsize=12)
+plt.ylim(0, 210)
+plt.tight_layout()
+plt.savefig(f"{folder_results}/{dataset_name}_{model_type}_{leakage_model}{hiding}.png",dpi=300, bbox_inches='tight')
+plt.close()
+# plt.show()
