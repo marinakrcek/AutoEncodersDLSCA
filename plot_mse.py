f5c1f82eb43c53f964e5a48fea1eb4a0798c22c5
Marina Krček <marina.krcek7@gmail.com>
2022-09-14 15:45:04 +0200

    improve plotting

--- plot_mse.py
+++ plot_mse.py
@@ -11,7 +11,7 @@ hiding = ""
 figure = plt.gcf()
 figure.set_size_inches(5, 4)
 
-for file_id in range(1, 9):
+for file_id in range(0,10):
     filepath = f"{folder_results}/{dataset_name}_{model_type}_{leakage_model}{hiding}_{file_id+1}.npz"
     npz_file = np.load(filepath, allow_pickle=True)
     # mse_ds = npz_file["mse_ds"].item()
@@ -19,14 +19,15 @@ for file_id in range(1, 9):
     dataset_parameters = npz_file["dataset"].item()
     epochs = dataset_parameters['epochs']
     assert epochs, len(history['loss'])
-    plt.plot(history['loss'], linewidth=1, label=f'loss-{file_id+1}')
+    plt.plot(history['loss'], linewidth=1, label=f'{file_id+1}')
     plt.plot(history['val_loss'], linewidth=1, color=plt.gca().lines[-1].get_color(), linestyle='--')
 
 plt.grid(True, which="both", ls="-")
 plt.xlabel("Epochs", fontsize=12)
 plt.ylabel("Loss", fontsize=12)
+plt.ylim(0,2)
 plt.tight_layout()
-plt.legend()
+plt.legend(loc='upper right', fontsize='small')
 plt.savefig(f"{folder_results}/{dataset_name}_{model_type}_{leakage_model}{hiding}.png",dpi=300, bbox_inches='tight')
 plt.close()
 
